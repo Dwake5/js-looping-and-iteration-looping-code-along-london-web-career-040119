74847c844430f3ca234e98ddf251f3fbf2c530c8
Dwake5 <wakeling101@hotmail.com>
2019-05-13 20:10:04 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, message) {
+    let newArr = []
+    for(let ele of arr) {
+        newArr.push( `Thank you, ${ele}, for the wonderful ${message} gift!`)
+    }
+    return newArr
+}
+
+function countdown(numb) {
+    while (numb >= 0) {
+        console.log(numb)
+    numb--
+    }
+}
